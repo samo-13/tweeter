3231222a0d9b65e284f7ec3af1743adf2492bf73
Sarah Moss <sarah-moss@live.com>
2022-05-24 18:19:41 +0000

    Add tweet container styles

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,7 @@ $(document).ready(function() { // don't execute anything until the DOM loads
   let maxCharacters = 140;
   
   $("#tweet-text-area").keyup(function() {
-    let characterLength = maxCharacters - $(this).val().length;
+    let characterLength = maxCharacters - $(this).val().length; // https://api.jquery.com/val/
     
   $(".counter").text(characterLength); // display the characterLength value in he counter 
   $(".counter").css("color", (characterLength < 0) ? "red" : "black"); // change counter text color to red if less than 0 remaining
