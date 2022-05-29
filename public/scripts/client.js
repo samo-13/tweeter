2f6a622822b7ff754f34522e5af2051c156114ef
Sarah Moss <sarah-moss@live.com>
2022-05-29 04:36:52 +0000

    Fix renderTweets + createTweetElement functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,67 +7,89 @@
 $(document).ready(function() {
 
     // Fake data taken from initial-tweets.json
-    const data = [
-      {
-        "user": {
-          "name": "Newton",
-          "avatars": "https://i.imgur.com/73hZDYK.png"
-          ,
-          "handle": "@SirIsaac"
-        },
-        "content": {
-          "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-        "created_at": 1461116232227
-      },
-      {
-        "user": {
-          "name": "Descartes",
-          "avatars": "https://i.imgur.com/nlhLi3I.png",
-          "handle": "@rd" },
-        "content": {
-          "text": "Je pense , donc je suis"
-        },
-        "created_at": 1461113959088
-      }
-    ]
+    // const data = [
+    //   {
+    //     "user": {
+    //       "name": "Newton",
+    //       "avatars": "https://i.imgur.com/73hZDYK.png"
+    //       ,
+    //       "handle": "@SirIsaac"
+    //     },
+    //     "content": {
+    //       "text": "If I have seen further it is by standing on the shoulders of giants"
+    //     },
+    //     "created_at": 1461116232227
+    //   },
+    //   {
+    //     "user": {
+    //       "name": "Descartes",
+    //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+    //       "handle": "@rd" },
+    //     "content": {
+    //       "text": "Je pense , donc je suis"
+    //     },
+    //     "created_at": 1461113959088
+    //   }
+    // ]
 
     // real data  from data/initial-tweet
 
+    const createTweetElement = (tweet) => {
+    // code creating the tweet element
+      const $tweet =
+        $(`
+        <article>
+          <header class="tweet-whole">
+            <div class="tweet-left">
+            <img id="user-icon" src="${tweet.user.avatars}">
+            <h2 id="user-name">${tweet.user.name}</h2>
+            </div>
+            <h2 id="user-handle">${tweet.user.handle}</h2>
+          </header>
+          <h3 class="tweet-content">${tweet.content.text}</h3>
+          <footer class="tweet">
+          <h3 id="day-count">${tweet.created_at}</h3>
+          <div id="social-icons">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>        
+            <i class="fa-solid fa-heart"></i>
+        </div>
+        </footer>
+      </article>
+      `)
+      console.log('createTweetElement tweet:', $tweet);
+      return $tweet;
+    };
+
+    const loadTweets = function() {
+      console.log('loadTweets function')
+
+          $.ajax({ 
+            url: '/tweets',
+            method: 'GET' 
+          })
+
+          .then(function(tweets) {
+            console.log('Success: See tweets', tweets);
+           renderTweets(tweets);
+          })
+    }
+    
+    loadTweets();
+
+
     const renderTweets = (tweets) => {
-      for(let i = 0; i < data.length; i++) { // loops through tweets
-        let tweet = data[i]
-        const $tweet = createTweetElement(tweet); // calls createTweetElement for each tweet
-        $('#tweets-container').append($tweet);// takes return value and appends it to the tweets container
+      console.log('renderTweets tweets:', tweets)
+      for(let i = 0; i < tweets.length; i++) { // loops through tweets
+        console.log('renderTweets tweets:', tweets);
+        console.log('renderTweets tweets[i]:', tweets[i]);
+        let tweet = tweets[i]
+        tweet = createTweetElement(tweet); // calls createTweetElement for each tweet
+        console.log('renderTweets createTweetElement tweet:', tweet);
+        $('#tweets-container').append(tweet);// takes return value and appends it to the tweets container
         // to add it to the page so we can make sure it's got all the right elements, classes, etc.
       }
     }
-
-    const createTweetElement = (tweet) => {
-    // code creating the tweet element
-
-    const $tweet =
-      $(`
-      <article>
-        <header class="tweet-whole">
-          <div class="tweet-left">
-          <img id="user-icon" src="${tweet.user.avatars}">
-          <h2 id="user-name">${tweet.user.name}</h2>
-          </div>
-          <h2 id="user-handle">${tweet.user.handle}</h2>
-        </header>
-        <h3 class="tweet-content">${tweet.content.text}</h3>
-        <footer class="tweet">
-        <h3 id="day-count">${tweet.created_at}</h3>
-        <div id="social-icons">
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>        
-          <i class="fa-solid fa-heart"></i>
-      </div>
-      </footer>
-    </article>
-    `)
-    };
 
     // set up event listener for submit event    
     // e = event handler
@@ -80,8 +102,8 @@ $(document).ready(function() {
       // jQuery .serialize() function turns a set of form data into a query string
       // the serialized data should be sent to the server in the data field of the AJAX POST request
       let tweet = $(this).serialize();
-      console.log(this.text);
-      console.log(this)
+      console.log('this.text:', this.text);
+      console.log('this:', this)
       console.log('tweet:', tweet)
       // ajax method sends the data to the server
       $.ajax({
@@ -93,16 +115,16 @@ $(document).ready(function() {
         data: tweet,
         // change the tweets html element to show the new tweet
         success: (tweet) => {
-          console.log('request succeeded:', tweet);
+          console.log('request succeeded see tweet:', tweet);
+          loadTweets();
         },
         error: (error) => {
           console.log('request failed:', error);
         }
     })
   })
+  loadTweets();
 });
-
-renderTweets(data);
 
 // Test / driver code (temporary). Eventually will get this from the server.
 // const tweetData = {
