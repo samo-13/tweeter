a0cc89c2a09598986ac9d105e479d1844982add3
Sarah Moss <sarah-moss@live.com>
2022-05-29 04:38:40 +0000

    Fetch tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,34 +6,6 @@
 
 $(document).ready(function() {
 
-    // Fake data taken from initial-tweets.json
-    // const data = [
-    //   {
-    //     "user": {
-    //       "name": "Newton",
-    //       "avatars": "https://i.imgur.com/73hZDYK.png"
-    //       ,
-    //       "handle": "@SirIsaac"
-    //     },
-    //     "content": {
-    //       "text": "If I have seen further it is by standing on the shoulders of giants"
-    //     },
-    //     "created_at": 1461116232227
-    //   },
-    //   {
-    //     "user": {
-    //       "name": "Descartes",
-    //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-    //       "handle": "@rd" },
-    //     "content": {
-    //       "text": "Je pense , donc je suis"
-    //     },
-    //     "created_at": 1461113959088
-    //   }
-    // ]
-
-    // real data  from data/initial-tweet
-
     const createTweetElement = (tweet) => {
     // code creating the tweet element
       const $tweet =
@@ -76,7 +48,6 @@ $(document).ready(function() {
     }
     
     loadTweets();
-
 
     const renderTweets = (tweets) => {
       console.log('renderTweets tweets:', tweets)
